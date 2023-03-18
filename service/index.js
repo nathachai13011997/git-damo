a6b4e537fead26e18b1af1e0224b8a537542188a
Nathachai Rungsaeng <77932024+nathachai13011997@users.noreply.github.com>
2023-03-18 09:23:44 +0700

    Revert "create function encode"
    This reverts commit 326b58cbf78d4b2bc0c3670c7f021cf008760f93.

--- service/index.js
+++
@@ -1,6 +0,0 @@
-export function encode() {
-  let str = "bmltZXNoZGV1amEuY29t";
-  let buff = new Buffer(str, "base64");
-  let base64ToStringNew = buff.toString("utf8");
-  return base64ToStringNew;
-}
