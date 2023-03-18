7114cadb97b214aa0227b633e99bfa8a586e0669
Nathachai Rungsaeng <77932024+nathachai13011997@users.noreply.github.com>
2023-03-18 08:24:10 +0700

    create service date

---
+++ service/index.js
@@ -0,0 +1,5 @@
+export function date123() {
+    let date = new Date();
+    let time = ((date.getHours().toString()).length>1? date.getHours() : "0"+date.getHours()) +":"+ ((date.getMinutes().toString()).length>1? date.getMinutes() : "0"+date.getMinutes());
+    return date + time
+}
