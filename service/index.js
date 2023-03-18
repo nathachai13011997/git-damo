93c715ee44bfaa3240ea89b01514a202d6c57e77
Nathachai Rungsaeng <77932024+nathachai13011997@users.noreply.github.com>
2023-03-18 09:31:21 +0700

    Revert "create function base64ToBase64url"
    This reverts commit 4273fcc73c5e251a0ba81023f00df9457d984d2d.

--- service/index.js
+++
@@ -1,7 +0,0 @@
-function base64ToBase64url(input) {
-    // Replace non-url compatible chars with base64url standard chars and remove leading =
-    return input
-      .replace(/\+/g, '_')
-      .replace(/\//g, '-')
-      .replace(/=+$/g, '');
-  }
